d3de5a16f3a31ef07383b2f1e09f2ae010095903
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-19 16:12:05 -0700

    add code for Loops exercises

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,6 +3,19 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+
+//        int numStart = 45;
+//        int numEnd = 50;
+//        while (numStart <= numEnd) {
+//            System.out.println(numStart);
+//            numStart++;
+//        }
+
+        for (int i = 2; i < 7; i++) {
+            if (i %2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
