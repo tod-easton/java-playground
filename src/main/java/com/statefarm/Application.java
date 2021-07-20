aad552b100e32f1592abe9f438b7b88c3117420b
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-19 20:11:14 -0700

    add logic to verify input arguments

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -3,31 +3,15 @@ package com.statefarm;
 public class Application {
 
     public static void main(String[] args) {
-//        System.out.println("Hello World");
 
-//        int numStart = 45;
-//        int numEnd = 50;
-//        while (numStart <= numEnd) {
-//            System.out.println(numStart);
-//            numStart++;
-//        }
-
-//        for (int i = 2; i < 7; i++) {
-//            if (i %2 == 0) {
-//                System.out.println(i);
-//            }
-//        }
-
-//        System.out.println(String.format("Hello %s", args[0]));
-
-        if (args.length != 2) {
-            System.out.println("You MUST pass 2 arguments");
+        if (args.length == 1) {
+            System.out.println("Please specify an email for " + args[0]);
             System.exit(1);
+        } else if (args.length != 2) {
+            System.out.println("Please specify a name and email");
+            System.exit(2);
+        } else {
+            System.out.println(args[0] + " <" + args[1] + ">");
         }
-
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-
-        System.out.println(a + b);
     }
 }
