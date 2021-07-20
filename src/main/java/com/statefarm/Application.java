9188b11dfe322bd2f847ed990f44f5e0de02295f
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-19 18:54:00 -0700

    add code for working with integers in Read Command Line Arguments

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -12,10 +12,22 @@ public class Application {
 //            numStart++;
 //        }
 
-        for (int i = 2; i < 7; i++) {
-            if (i %2 == 0) {
-                System.out.println(i);
-            }
+//        for (int i = 2; i < 7; i++) {
+//            if (i %2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+//        System.out.println(String.format("Hello %s", args[0]));
+
+        if (args.length != 2) {
+            System.out.println("You MUST pass 2 arguments");
+            System.exit(1);
         }
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println(a + b);
     }
 }
