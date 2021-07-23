9a110ae0944f41bfa22b7b845335bc67f037a812
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-23 10:43:21 -0700

    add code for Accumulator challenge

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -1,5 +1,9 @@
 package com.statefarm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -20,6 +24,7 @@ public class Application {
 
 //        System.out.println(String.format("Hello %s", args[0]));
 
+        // testing Integer.parseInt
         if (args.length != 2) {
             System.out.println("You MUST pass 2 arguments");
             System.exit(1);
@@ -30,4 +35,42 @@ public class Application {
 
         System.out.println(a + b);
     }
+
+    // Implement the sortArray method below below
+//    public ArrayList<String> sortArray(String[] inputArray) {
+//        ArrayList<String> newAL = new ArrayList<>(Arrays.asList(inputArray));
+//        newAL.sort(null);
+//        return newAL;
+//    }
+
+    //For the given method signature, write an implementation that generates a HashMap, adds the following two items, and returns this new HashMap.
+//    public HashMap<Integer, String> genHashMap() {
+//        HashMap<Integer, String> numbers = new HashMap<>();
+//        numbers.put(747, "Airplane");
+//        numbers.put(10, "Laphroaig");
+//        return numbers;
+//    }
+
+// Accumulator challenge
+//    Input [""] should return ""
+//    Input [] should return ""
+//    Input ["Alice", "Bob"] should return "Alice and Bob"
+//    Input ["Alice", "Bob", "Carol"] should return "Alice, Bob and Carol"
+    String toSentence(String[] array) {
+        String result = "";
+        if(array.length == 0) {  // array[0].isEqual() to compare if element is String
+            return result;
+        }
+        result += array[0];
+        for(int i = 1; i < array.length; i++) {
+            if (i == array.length -1){
+                result += " and " + array[i];
+            } else {
+                result += ", " + array[i];
+            }
+        }
+        return result;
+    }
+
+
 }
